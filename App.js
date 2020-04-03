15fd1d77c2e86a7eecdbd1de91a0a3f2f59942a0
github username <github email address>
2020-04-03 15:02:35 -0600

    User Logging in

--- App.js
+++ App.js
@@ -1,11 +1,19 @@
-import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, View, Text, Button } from 'react-native';
 import Header from './components/Header';
+import Login from './components/Login';
 
 export default function App() {
+  const [showLogin, setShowLogin] = useState(false)
+
+  const cancelLogin = () => {
+    setShowLogin(false)
+  }
   return (
     <View style={styles.main}>
       <Header />
+      <Button style={styles.button}title="Login" onPress={()=> setShowLogin(true)}/> 
+      <Login cancelLogin={cancelLogin} showLogin={showLogin}/>
     </View>
   );
 }
@@ -13,5 +21,12 @@ export default function App() {
 const styles = StyleSheet.create({
   main:{
     flex: 1
+  },
+  button:{
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    padding: 4,
+    margin: 4,
+    width: '75%',
   }
 });
--- components/Login.js
+++ components/Login.js
@@ -1,35 +1,68 @@
-import React, { useState } from 'react';
-import { View, TextInput, StyleSheet, Button, Modal } from 'react-native'
+import React, { useState, Component } from 'react';
+import { View, TextInput, StyleSheet, Button, Modal, AsyncStorage } from 'react-native'
 
-export default function Login (props ){
+const test = {username:'test', password:'123'}
+export default function Login (props){
+    const userURL = `http://localhost:3000/users`
+    const loginURL = `http://localhost:3000/login`
     const [username, setusername] = useState('')
     const [password, setpassword] = useState('')
+    const [userInfo, setuserInfo] = useState('')
 
     const usernameInputHandler = (enteredText) =>{
-        console.log(enteredText)
+        setusername(enteredText)
     }
 
     const passwordInputHandler = (enteredText) =>{
-        console.log(enteredText)
+        setpassword(enteredText)
     }
+
+    const signin = async () => {
+        if(test.username === username && test.password === password){
+            alert("Logged in")
+            await AsyncStorage.setItem('logged', true)
+        }else{
+            alert("Username or password are incorrect!")
+        }
+    };
+    
     return( 
-        <Modal visible={props.visible} animationType='slide'>
-            <View >
-                <TextInput placeholder="Enter Username" 
+        <Modal visible={props.showLogin} animationType='slide'>
+            <View style={styles.login}>
+                <TextInput style={styles.input} placeholder="Enter Username" 
                 onChangeText={usernameInputHandler}
                 value={username}/>
 
-                <TextInput placeholder="Enter Password" 
+                <TextInput style={styles.input} placeholder="Enter Password" 
                 onChangeText={passwordInputHandler}
-                value={password}/>
+                value={password} secureTextEntry={true}/>
 
                 <View style={styles.button}>
-                    <Button title='Cancel' color='red' onPress={props.cancelGoal}/>
-                    <Button title='Submit' onPress={goalHandler}/>
+                    <Button title='Cancel' color='red' onPress={props.cancelLogin}/>
+                    <Button title='Submit' onPress={()=> signin()}/>
                 </View>
             </View>
         </Modal>
     );
+};
+
 const styles = StyleSheet.create({
-   
-})
+    button:{
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        padding: 4,
+        width: '75%',
+    },
+    input:{
+        borderColor:'black', 
+        borderWidth: 1, 
+        padding:10, 
+        width:'80%',
+        marginBottom: 10
+      },
+    login:{
+        flex: 1,
+        justifyContent:'center', 
+        alignItems:'center'
+      }
+ });
